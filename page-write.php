417afa783c5c536186c7c03ff154ea5396a996ca
Harrisonfm <john@harrisonfm.com>
2016-02-29 13:23:34 -0500

    Post Refinements

--- page-write.php
+++ page-write.php
@@ -3,7 +3,7 @@
 Template Name: Write
 */
 get_header();
-$query = new WP_Query('post_type=post');
+$query = new WP_Query(array('post_type'=>'post'));
 ?>
 <div class="page">
 	<?php get_template_part('nav', 'write') ?>
--- single.php
+++ single.php
@@ -8,22 +8,57 @@ get_header();
 		#get_template_part('loader');
 		if (have_posts()) :
 			while (have_posts()):the_post();
-				$featImg = get_post_thumbnail_id($post->ID);
+				$id = $post->ID;
+				$featImg = get_post_thumbnail_id($id);
 				$category = get_the_category();
 				$category = $category[0];
 				$wide = wp_get_attachment_image_src($featImg, 'wide')[0];
 				$large = wp_get_attachment_image_src($featImg, 'large')[0];
 				?>
+				<div id="banner" data-url-wide="<?= $wide ?>" data-url-large="<?= $large ?>"></div>
 				<header>
 					<h1><?php the_title() ?></h1>
-					<p><?php the_time('M jS, Y') ?>, <a href="<?= get_category_link($category->cat_ID) ?>"><?= $category->name ?></a></p>
+					<p><?php the_time('M jS, Y') ?> in <a href="<?= get_category_link($category->cat_ID) ?>"><?= $category->name ?></a></p>
 				</header>
-				<div id="banner" data-url-wide="<?= $wide ?>" data-url-large="<?= $large ?>"></div>
 				<main>
 				<? the_content();
 			endwhile;
 		endif; ?>
 			</main>
+			<footer>
+			<?php
+			$related = new WP_Query(
+				array(
+				'cat'=>$category->cat_ID,
+				'posts_per_page' => 3,
+				'post__not_in' => array($id)
+			));
+			if ($related->have_posts()) : ?>
+					<h5>Other Reading</h5>
+					<?php while ($related->have_posts()):
+						$related->the_post();
+					?>
+					<a href="<?= the_permalink() ?>">
+						<article>
+							<div>
+								<h5><?= the_title() ?></h5>
+								<p><?= the_excerpt() ?></p>
+							</div>
+							<figure>
+								<?= the_post_thumbnail('thumb') ?>
+							</figure>
+						</article>
+					</a>
+			<?php
+			endwhile;
+			endif;
+			?>
+			<div id="end">
+				<p>&copy; John Harrison <?= date("Y") ?></p>
+				<a href="/about"><p>About</p></a>
+				<a href="#" class="top"><p>Top</p></a>
+			</div>
+			</footer>
 	</section>
 </div>
 <?php get_footer();
