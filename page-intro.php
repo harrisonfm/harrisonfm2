14980fd96f2b7db11fac360f173ded4e1d7d2222
Harrisonfm <john@harrisonfm.com>
2016-10-11 15:44:23 -0400

    Recent Posts on Intro

--- page-intro.php
+++ page-intro.php
@@ -12,6 +12,29 @@ $bg = array(
 <div class="page" data-url-large="<?= $bg['large'][0] ?>" data-url-full="<?= $bg['full'][0] ?>">
 	<?php get_template_part('loader') ?>
 	<div class="logo">Harrison <span class="icon-bolt-before"></span>FM<span class="icon-bolt-after"></span></div>
-	<?php get_template_part('nav') ?>
+	<?php get_template_part('nav');
+	$args = array(
+    'posts_per_page' => 3
+	);
+	$recents = new WP_Query($args);
+	if($recents->have_posts()){
+		?>
+		<section class="recent">
+		<h5>Latest Articles:</h5>
+		<?php
+		  while($recents->have_posts()){
+		    $recents->the_post();
+		    $featImg = get_post_thumbnail_id($post->ID);
+		    $thumbnail = wp_get_attachment_image_src($featImg, 'thumbnail')[0];
+		    echo '<a href="'.get_permalink().'">
+		      <article>
+		      	<img src="'.$thumbnail.'" />
+		        <p>'.get_the_title().'</p>
+		      </article>
+		    </a>';
+		  }
+			?>		
+		</section>
+	<?php	}	?>
 </div>
 <?php get_footer() ?>
