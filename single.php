4855b8e08a2d3d2544839f90b82a402143bb1d00
Harrisonfm <john@harrisonfm.com>
2017-01-24 16:59:58 -0500

    ACF Photo Credit

--- single.php
+++ single.php
@@ -14,62 +14,74 @@ get_header();
 				$category = $category[0];
 				$wide = wp_get_attachment_image_src($featImg, 'wide')[0];
 				$large = wp_get_attachment_image_src($featImg, 'large')[0];
+
 				?>
 				<div id="banner" data-url-wide="<?= $wide ?>" data-url-large="<?= $large ?>"></div>
 				<header>
 					<h1><?php the_title() ?></h1>
-					<p><?php the_time('M jS, Y') ?> in <a href="<?= get_category_link($category->cat_ID) ?>"><?= $category->name ?></a></p>
-					<?php #if($photoCredit = get_post_meta($post->ID, 'photo_credit', true)){ ?>
+					<p>
+						<?php the_time('M jS, Y') ?> in <a href="<?= get_category_link($category->cat_ID) ?>"><?= $category->name ?></a>
+						<?php if($credit = get_field('photo_credit')): ?>
+							<span class="credit"><i class="icon icon-image"></i> via 
+								<?php if($creditURL = get_field('photo_credit_url')): ?>
+									<a href="<?= $creditURL ?>" target="_blank"><?= $credit ?></a>
+								<?php else: 
+									echo $credit;
+								endif;
+								?>
+							</span>
+						<?php endif; ?>
+					</p>
 				</header>
-				<div id="content">
-				<? the_content();
+				<div id="content"><?php the_content(); ?></div>
+				<?php
 			endwhile;
-		endif; ?>
-			</div id="content">
-			<footer>
-				<?php
-				wp_reset_query();
-				$tags = wp_get_post_tags($post->ID);
-				if($tags){
-					$tagLine = '<p class="tag-line"><span>Tagged:</span> ';
-					foreach($tags as $tag){
-						$tagLine .= '<a href="'.get_tag_link($tag->term_id).'">#'.$tag->name.'</a>, ';
-					}
-					echo substr($tagLine, 0, -2).'</p>';
+		endif; 
+		?>
+		<footer>
+			<?php
+			wp_reset_query();
+			$tags = wp_get_post_tags($post->ID);
+			if($tags){
+				$tagLine = '<p class="tag-line"><span>Tagged:</span> ';
+				foreach($tags as $tag){
+					$tagLine .= '<a href="'.get_tag_link($tag->term_id).'">#'.$tag->name.'</a>, ';
 				}
-				$related = new WP_Query(
-					array(
-					'cat'=>$category->cat_ID,
-					'posts_per_page' => 3,
-					'post__not_in' => array($id)
-				));
-				if ($related->have_posts()) : ?>
-						<h5>Other Reading</h5>
-						<?php while ($related->have_posts()):
-							$related->the_post();
-						?>
-						<a href="<?= the_permalink() ?>">
-							<article>
-								<div>
-									<h5><?= the_title() ?></h5>
-									<p><?= the_excerpt() ?></p>
-								</div>
-								<figure>
-									<?= the_post_thumbnail('thumb') ?>
-								</figure>
-							</article>
-						</a>
-				<?php
-				endwhile;
-				endif;
-				?>
-				<div id="end">
-					<p>&copy; John Harrison, <?= date("Y") ?></p>
-					<a href="/about"><p>About</p></a>
-					<a href="/write"><p>Writing</p></a>
-					<p class="top link">Top</p>
-				</div>
-			</footer>
+				echo substr($tagLine, 0, -2).'</p>';
+			}
+			$related = new WP_Query(
+				array(
+				'cat'=>$category->cat_ID,
+				'posts_per_page' => 3,
+				'post__not_in' => array($id)
+			));
+			if ($related->have_posts()) : ?>
+					<h5>Other Reading</h5>
+					<?php while ($related->have_posts()):
+						$related->the_post();
+					?>
+					<a href="<?= the_permalink() ?>">
+						<article>
+							<div>
+								<h5><?= the_title() ?></h5>
+								<p><?= the_excerpt() ?></p>
+							</div>
+							<figure>
+								<?= the_post_thumbnail('thumb') ?>
+							</figure>
+						</article>
+					</a>
+			<?php
+			endwhile;
+			endif;
+			?>
+			<div id="end">
+				<p>&copy; John Harrison, <?= date("Y") ?></p>
+				<a href="/about"><p>About</p></a>
+				<a href="/write"><p>Writing</p></a>
+				<p class="top link">Top</p>
+			</div>
+		</footer>
 	</main>
 	<div id="slides">
 		<footer>
