722150392fa9b033b414c418b14f8213dbaae791
Harrisonfm <john@harrisonfm.com>
2017-03-25 17:53:27 -0400

    About Banners to <img>

--- assets/js/src/about.js
+++ assets/js/src/about.js
@@ -6,30 +6,15 @@ _ = require('lodash');
 
 module.exports = class About {
 	constructor(){
-		this.$figures = $('figure');
-		this.$banner = $('.banner');
 		this.$nav = $('nav');
+		this.$figures = $('figure');
 
-		this.loader = new Loader($('main'), this.$figures.length + 1);
+		this.loader = new Loader($('main'), this.$figures.length);
 		this.$figures.find('img').filter((idx, el) => {
 		    return el.complete;
 		}).each(() => this.loader.increment()).end().on('load', () => this.loader.increment());
 
-		const img = window.innerWidth <= 400 ? this.$banner.attr('data-url-wide') : this.$banner.attr('data-url-large');
-		this.$banner.css('backgroundImage', "url('"+img+"')");
-		$('<img/>').attr('src', img).on('load', () => this.loader.increment());
-
-		$(window).on('resize', _.debounce(() => this.resizeBanner(), 300));
 		$(window).on('scroll', _.debounce(() => this.showNav(), 300));
-	}
-
-	resizeBanner(){
-		if((this.lastWindowWidth <= 400 && window.innerWidth > 400) || (this.lastWindowWidth > 400 && window.innerWidth <= 400)){
-			const img = window.innerWidth <= 400 ? this.$banner.attr('data-url-wide') : this.$banner.attr('data-url-large');
-			this.$banner.css('backgroundImage', "url('"+img+"')");
-		}
-
-		this.lastWindowWidth = window.innerWidth;
 	}
 
 	showNav(){
--- page-about.php
+++ page-about.php
@@ -11,9 +11,15 @@ $me = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full', true);
 	<?php get_template_part('nav') ?>
 	<?php if(have_posts()): ?>
 		<main>
-			<div class="banner" data-url-wide="<?= $banner['sizes']['wide'] ?>" data-url-large="<?= $banner['sizes']['large'] ?>"></div>
-			<?php get_template_part('loader') ?>
 			<figure>
+				<picture>
+	  			<source media="(max-width: 400px)" srcset="<?= $banner['sizes']['wide'] ?>" />
+	  			<source media="(min-width: 401px) and (max-width: 899px)" srcset="<?= $banner['sizes']['large'] ?>" />
+	  			<img class="banner" src="<?= $banner['url'] ?>" />
+	  		</picture>
+	  	</figure>
+			<?php get_template_part('loader') ?>
+			<figure id="me">
 				<img src="<?= $me[0] ?>" />
 			</figure>
 			<?php while(have_posts()):
